c74e22167c3cb990bde7f6ec24b7093755aa1885
bolcof <sanctum.crusade.ex@gmail.com>
2024-04-18 11:48:27 +0900

    CollsionにDrawするために.hも必要だった

--- JunkYard/headers/main/collisions.h
+++ JunkYard/headers/main/collisions.h
@@ -2,6 +2,7 @@
 #define COLLISIONS_HEADER set
 
 
+void DrawNumber3(uint8_t x,uint8_t y, uint16_t number,uint8_t digits);
 void BounceAgainstTheWalls();
 void CollidePaddleAgainstBall();
 void CollideBricksAgainstBall();
