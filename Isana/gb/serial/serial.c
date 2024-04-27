5fdc8295e8f392781f100ab74a3abbb4f4aba939
IsanaKoto <isn.kumano@gmail.com>
2024-04-27 15:06:35 +0900

    refactor & ページ切り替え時にスプライトを消去する

--- Isana/gb/serial/serial.c
+++ Isana/gb/serial/serial.c
@@ -21,13 +21,49 @@ void setupArrowSprite() {
     set_sprite_tile(0, 0);
     move_sprite(0, 8, 48);
 }
-// void clearScreen() {
-//     display_off();  // 画面表示をオフにしてちらつきを防ぐ
-//     printf("\x1b[2J");  // 画面をクリア
-//     DISPLAY_ON;  // 画面表示をオンにする
-// }
 
-void drawScreen() {
+void updateArrowPosition() {
+    move_sprite(0, 8, 48 + 8 * (currentSelection % ssidsPerPage));
+}
+
+void hideAllSprites() {
+    for (uint8_t i = 0; i < 40; i++) {  // Game Boyは最大で40個のスプライトを扱えます
+        move_sprite(i, 0, 0);  // スプライトを画面外に移動
+        hide_sprite(i);  // スプライトを非表示にする
+    }
+}
+
+void updateSelection(uint8_t joypadState) {
+    uint8_t oldSelection = currentSelection;
+    if (joypadState & J_UP && currentSelection > 0) currentSelection--;
+    if (joypadState & J_DOWN && currentSelection < num_ssids - 1) currentSelection++;
+    if (joypadState & J_RIGHT && currentPage < (num_ssids - 1) / ssidsPerPage) {
+        currentPage++;
+        drawMainMenu();
+    }
+    if (joypadState & J_LEFT && currentPage > 0) {
+        currentPage--;
+        drawMainMenu();
+    }
+    if (oldSelection != currentSelection) {
+        updateArrowPosition();
+    }
+}
+
+// 画面をクリアする関数
+void clearScreen() {
+    display_off();  // 画面更新のちらつきを防ぐために表示をオフにする
+    for (int y = 0; y < 18; y++) {
+        for (int x = 0; x < 20; x++) {
+            // gotoxy(x, y);
+            putchar(' ');  // 空白で画面をクリア
+        }
+    }
+    DISPLAY_ON;  // 画面表示を再開する
+}
+
+// メインメニューを描画する関数
+void drawMainMenu() {
     printf("\n");
     printf("%s\n\n", ip_address);
     printf("SSID\n");
@@ -47,62 +83,11 @@ void drawScreen() {
     SHOW_SPRITES;
 }
 
-void updateArrowPosition() {
-    move_sprite(0, 8, 48 + 8 * (currentSelection % ssidsPerPage));
-}
-
-void updateSelection(uint8_t joypadState) {
-    uint8_t oldSelection = currentSelection;
-    if (joypadState & J_UP && currentSelection > 0) currentSelection--;
-    if (joypadState & J_DOWN && currentSelection < num_ssids - 1) currentSelection++;
-    if (joypadState & J_RIGHT && currentPage < (num_ssids - 1) / ssidsPerPage) {
-        currentPage++;
-        drawScreen();
-    }
-    if (joypadState & J_LEFT && currentPage > 0) {
-        currentPage--;
-        drawScreen();
-    }
-    if (oldSelection != currentSelection) {
-        updateArrowPosition();
-    }
-}
-
-void selectSSID() {
-    // SSIDの所在ページを計算して現在のページを更新
-    clearScreen();
-    printf("\nSSID: %s\n", ssids[currentSelection]);
-}
-
-
-// 画面をクリアする関数
-void clearScreen() {
-    display_off();  // 画面更新のちらつきを防ぐために表示をオフにする
-    for (int y = 0; y < 18; y++) {
-        for (int x = 0; x < 20; x++) {
-            // gotoxy(x, y);
-            putchar(' ');  // 空白で画面をクリア
-        }
-    }
-    DISPLAY_ON;  // 画面表示を再開する
-}
-
-// メインメニューを描画する関数
-void drawMainMenu() {
-    clearScreen();
-    printf("Main Menu:\n");
-    printf("1. Start Game\n");
-    printf("2. SELECTED\n");
-    printf("3. Exit\n");
-}
-
 // オプションメニューを描画する関数
 void drawSelectedSSID() {
     clearScreen();
-    printf("SELECTED Menu:\n");
-    printf("1. Sound: ON\n");
-    printf("2. Difficulty: HARD\n");
-    printf("3. Back\n");
+    // SSIDの所在ページを計算して現在のページを更新
+    printf("\nSSID: %s\n", ssids[currentSelection]);
 }
 
 
@@ -114,6 +99,7 @@ enum ScreenState {
 
 // 画面を切り替える関数
 void switchScreen(enum ScreenState newScreen) {
+    hideAllSprites();
     currentScreen = newScreen;
     switch (currentScreen) {
         case MAIN_MENU:
@@ -128,7 +114,7 @@ void switchScreen(enum ScreenState newScreen) {
 void main(void) {
     DISPLAY_ON;
     setupArrowSprite();
-    drawScreen();
+    drawMainMenu();
     updateArrowPosition();
 
     while (1) {
@@ -137,7 +123,7 @@ void main(void) {
             updateSelection(joypadState);
         }
         if (joypadState & J_A) {
-            selectSSID();
+            switchScreen(SELECTEDSSID);
         }
         delay(100);
     }
